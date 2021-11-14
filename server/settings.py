c1d9c9a0e2edf41d1ecacf505ab6975ef7a7cb46
Poofigator <egor2002i@mail.ru>
2021-11-14 16:19:02 +0400

    requirements.txt

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p+xi0bsi$8jf!%whj%e4)x*l2n3vin!5iew8lq$3-d(-7#*-=^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
